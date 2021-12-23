32caf8c3166f1248b057dce59f6f1d87e8a5def6
mohan76 <mohan.hsieh@gmail.com>
2021-12-23 12:03:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-TRAIN_DIR = "data/train"
-VAL_DIR = "data/val"
+TRAIN_DIR = "../drive/MyDrive/data/cyclegan/images/horsetozebra/train"
+VAL_DIR = "../drive/MyDrive/data/cyclegan/images/horsetozebra/val"
 BATCH_SIZE = 1
 LEARNING_RATE = 1e-5
 LAMBDA_IDENTITY = 0.0
@@ -13,10 +13,10 @@ NUM_WORKERS = 4
 NUM_EPOCHS = 10
 LOAD_MODEL = True
 SAVE_MODEL = True
-CHECKPOINT_GEN_H = "genh.pth.tar"
-CHECKPOINT_GEN_Z = "genz.pth.tar"
-CHECKPOINT_CRITIC_H = "critich.pth.tar"
-CHECKPOINT_CRITIC_Z = "criticz.pth.tar"
+CHECKPOINT_GEN_H = "../drive/MyDrive/data/cyclegan/pretrained/genh.pth.tar"
+CHECKPOINT_GEN_Z = "../drive/MyDrive/data/cyclegan/pretrained/genz.pth.tar"
+CHECKPOINT_CRITIC_H = "../drive/MyDrive/data/cyclegan/pretrained/critich.pth.tar"
+CHECKPOINT_CRITIC_Z = "../drive/MyDrive/data/cyclegan/pretrained/criticz.pth.tar"
 
 transforms = A.Compose(
     [
