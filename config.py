90060644fd03eb256bfc33c1789b5239fc5ece49
mohan76 <mohan.hsieh@gmail.com>
2021-12-23 16:38:15 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-TRAIN_DIR = "../drive/MyDrive/data/cyclegan/images/horsetozebra/train"
-VAL_DIR = "../drive/MyDrive/data/cyclegan/images/horsetozebra/val"
+TRAIN_DIR = "../drive/MyDrive/data/cyclegan/images/monet/train"
+VAL_DIR = "../drive/MyDrive/data/cyclegan/images/monet/val"
 BATCH_SIZE = 1
 LEARNING_RATE = 1e-5
 LAMBDA_IDENTITY = 0.0
